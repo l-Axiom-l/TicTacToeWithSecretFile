d2c05b0f863cce2368f8b9341067bb6292d63706
l_Axiom_l <axiom@mail.gmx>
2022-03-08 13:50:59 +0100

    Add project files.

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.Board = new System.Windows.Forms.GroupBox();
+            this.console = new System.Windows.Forms.TextBox();
+            this.display = new System.Windows.Forms.Label();
+            this.restart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Board.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.A1.Location = new System.Drawing.Point(2, 9);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(87, 87);
+            this.A1.TabIndex = 0;
+            this.A1.Text = "\r\n";
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.Click);
+            // 
+            // A2
+            // 
+            this.A2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.A2.Location = new System.Drawing.Point(102, 9);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(87, 87);
+            this.A2.TabIndex = 0;
+            this.A2.Text = "\r\n";
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.Click);
+            // 
+            // A3
+            // 
+            this.A3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.A3.Location = new System.Drawing.Point(202, 9);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(87, 87);
+            this.A3.TabIndex = 1;
+            this.A3.Text = "\r\n";
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.Click);
+            // 
+            // B1
+            // 
+            this.B1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.B1.Location = new System.Drawing.Point(2, 109);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(87, 87);
+            this.B1.TabIndex = 0;
+            this.B1.Text = "\r\n";
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.Click);
+            // 
+            // B2
+            // 
+            this.B2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.B2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B2.Location = new System.Drawing.Point(102, 109);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(87, 87);
+            this.B2.TabIndex = 0;
+            this.B2.Text = "\r\n";
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.Click);
+            // 
+            // B3
+            // 
+            this.B3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.B3.Location = new System.Drawing.Point(202, 109);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(87, 87);
+            this.B3.TabIndex = 1;
+            this.B3.Text = "\r\n";
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.Click);
+            // 
+            // C3
+            // 
+            this.C3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.C3.Location = new System.Drawing.Point(202, 209);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(87, 87);
+            this.C3.TabIndex = 4;
+            this.C3.Text = "\r\n";
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.Click);
+            // 
+            // C2
+            // 
+            this.C2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.C2.Location = new System.Drawing.Point(102, 209);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(87, 87);
+            this.C2.TabIndex = 2;
+            this.C2.Text = "\r\n";
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.Click);
+            // 
+            // C1
+            // 
+            this.C1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.C1.Location = new System.Drawing.Point(2, 209);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(87, 87);
+            this.C1.TabIndex = 3;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.Click);
+            // 
+            // Board
+            // 
+            this.Board.Controls.Add(this.B3);
+            this.Board.Controls.Add(this.C3);
+            this.Board.Controls.Add(this.A1);
+            this.Board.Controls.Add(this.C2);
+            this.Board.Controls.Add(this.A2);
+            this.Board.Controls.Add(this.C1);
+            this.Board.Controls.Add(this.B1);
+            this.Board.Controls.Add(this.A3);
+            this.Board.Controls.Add(this.B2);
+            this.Board.Location = new System.Drawing.Point(21, 81);
+            this.Board.Name = "Board";
+            this.Board.Size = new System.Drawing.Size(291, 300);
+            this.Board.TabIndex = 5;
+            this.Board.TabStop = false;
+            // 
+            // console
+            // 
+            this.console.Location = new System.Drawing.Point(332, 90);
+            this.console.Multiline = true;
+            this.console.Name = "console";
+            this.console.ReadOnly = true;
+            this.console.Size = new System.Drawing.Size(130, 291);
+            this.console.TabIndex = 6;
+            // 
+            // display
+            // 
+            this.display.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.display.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.display.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.display.Location = new System.Drawing.Point(20, 31);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(290, 29);
+            this.display.TabIndex = 7;
+            this.display.Text = "Round : 0";
+            this.display.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.display.Click += new System.EventHandler(this.display_Click);
+            // 
+            // restart
+            // 
+            this.restart.Location = new System.Drawing.Point(332, 31);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(130, 29);
+            this.restart.TabIndex = 8;
+            this.restart.Text = "New Game";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 384);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 383);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 411);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.console);
+            this.Controls.Add(this.Board);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Board.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.GroupBox Board;
+        private System.Windows.Forms.TextBox console;
+        private System.Windows.Forms.Label display;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using EasyEncrypt2;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool Player1 = true;
+        int counter = 0;
+        int resetCounter = 0;
+        List<string> Rules = new List<string>();
+        List<string> Player1List = new List<string>();
+        List<string> Player2List = new List<string>();
+        string P1 = "A1 A3 C1 C3 B2";
+        string P2 = "A2 B1 B3 C2";
+        EasyEncrypt crypto;
+        SymmetricAlgorithm algo = SymmetricAlgorithm.Create();
+        bool unlock = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Application.ApplicationExit += (s, e) => Lock();
+            console.KeyDown += (s, e) => { if (e.KeyCode == Keys.F1) Confirm(); };
+            crypto = new EasyEncrypt("5642", "Y9iGr@dya?)7", algo);
+            Properties.Settings.Default.Reload();
+            Rules = Properties.Resources.TicTacToeRules.Split('\n').ToList();
+        }
+
+        void ChangeField(int Index, Image image, ref List<string> list)
+        {
+            IEnumerable<Button> buttons = Board.Controls.OfType<Button>().Where(x => x.GetHashCode().Equals(Index));
+
+            if (Player1List.Contains(buttons.ElementAt(0).Name) || Player2List.Contains(buttons.ElementAt(0).Name))
+                return;
+
+            buttons.ElementAt(0).BackgroundImage = image;
+            list.Add(buttons.ElementAt(0).Name);
+        }
+
+        private void Click(object sender, EventArgs e)
+        {
+            counter++;
+            resetCounter = 0;
+            display.Text = "Round : " + counter;
+            if (Player1)
+            {
+                ChangeField(sender.GetHashCode(), Properties.Resources.Cross, ref Player1List);
+                console.AppendText("Player1 : " + Player1List.Last() + Environment.NewLine);
+                Check(Player1List);
+            }
+            else
+            {
+                ChangeField(sender.GetHashCode(), Properties.Resources.Circle, ref Player2List);
+                console.AppendText("Player2 : " + Player2List.Last() + Environment.NewLine);
+                Check(Player2List);
+            }
+
+
+            Player1 = !Player1;
+        }
+        void Check(List<string> list)
+        {
+            foreach (string item in Rules)
+            {
+                string[] split = item.Split(' ');
+                if (list.Contains(split[0]) && list.Contains(split[1]) && list.Contains(split[2]))
+                    Win();
+            }
+        }
+        void Win()
+        {
+            string player;
+            if (Player1) player = "Player1"; else player = "Player2";
+            Board.Enabled = false;
+            display.ForeColor = Color.Green;
+            display.Text = player + " Won";
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            resetCounter++;
+            Board.Enabled = true;
+            display.ForeColor = Color.Black;
+            counter = 0;
+            display.Text = "Round : " + counter;
+
+            foreach (Button button in Board.Controls.OfType<Button>()) button.BackgroundImage = null;
+            Player1List.Clear();
+            Player2List.Clear();
+            Player1 = true;
+            console.ResetText();
+            console.ReadOnly = true;
+            Lock();
+
+            if (resetCounter > 10)
+                Reset();
+        }
+
+        private void display_Click(object sender, EventArgs e)
+        {
+            string[] split = P1.Split(' ');
+            string[] split2 = P2.Split(' ');
+            if (Player1List.All(x => split.Contains(x)) && Player2List.All(x => split2.Contains(x)) && Player1List.Count > 0 && Player2List.Count > 0)
+            {
+                console.ResetText();
+                console.ReadOnly = false;
+                Unlock();
+            }
+        }
+
+        void Unlock()
+        {
+            unlock = true;
+
+            if (Properties.Settings.Default.Setting == null)
+                return;
+
+
+            string temp = Properties.Settings.Default.Setting;
+            console.Text = temp;
+            temp = crypto.Decrypt(temp);
+            Properties.Settings.Default.Setting = temp;
+            console.Text = Properties.Settings.Default.Setting;
+
+
+        }
+
+        void Lock()
+        {
+            if (!unlock)
+                return;
+
+            string temp = crypto.Encrypt(Properties.Settings.Default.Setting);
+            Properties.Settings.Default.Setting = temp;
+            MessageBox.Show(temp, "Attention", MessageBoxButtons.OK);
+            Properties.Settings.Default.Save();
+        }
+
+
+        void Confirm()
+        {
+            if (!unlock)
+                return;
+
+            Properties.Settings.Default.Setting = console.Text;
+            MessageBox.Show("Saved");
+        }
+
+        void Reset()
+        {
+            console.Text = "Reset activated";
+            Properties.Settings.Default.Setting = null;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Reload();
+            Properties.Settings.Default.Setting = crypto.Decrypt(Properties.Settings.Default.Setting);
+            console.Text = Properties.Settings.Default.Setting;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Setting = crypto.Encrypt(Properties.Settings.Default.Setting);
+            console.AppendText(Properties.Settings.Default.Setting);
+            Properties.Settings.Default.Save();
+        }
+    }
+}
